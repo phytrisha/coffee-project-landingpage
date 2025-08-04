176b7944545bc38847777beabebd00601a4c97dd
Mark Meyer <12794995+phytrisha@users.noreply.github.com>
2025-08-04 19:43:43 +0200

    Updated subscription models

--- components/sections/subscriptions.tsx
+++ components/sections/subscriptions.tsx
@@ -3,9 +3,9 @@ import SubscriptionCard from '../subscription-card';
 
 const SubscriptionSection = () => {
 
-    const espressoFeatures = ['5 Credits pro Monat', 'Bei allen teilnehmenden Cafés einlösbar', 'Bis zu 3 Credits auf den nächsten Monat übertragbar', 'Teile Credits mit Freunden']
-    const cortadoFeatures = ['15 Credits pro Monat', 'Bei allen teilnehmenden Cafés einlösbar', 'Bis zu 5 Credits auf den nächsten Monat übertragbar', 'Teile Credits mit Freunden', '5% Rabatt auf Snacks']
-    const cappuccinoFeatures = ['30 Credits pro Monat', 'Bei allen teilnehmenden Cafés einlösbar', 'Bis zu 10 Credits auf den nächsten Monat übertragbar', 'Teile Credits mit Freunden', '10% Rabatt auf Snacks', 'Aufladen von Credits im laufenden Monat']
+    const espressoFeatures = ['5 Kaffees pro Monat', 'Bei allen teilnehmenden Cafés einlösbar', 'Bis zu 3 Kaffees auf den nächsten Monat übertragbar']
+    const cortadoFeatures = ['10 Kaffees pro Monat', 'Bei allen teilnehmenden Cafés einlösbar', 'Bis zu 5 Kaffees auf den nächsten Monat übertragbar']
+    const cappuccinoFeatures = ['1 Kaffees pro Tag', 'Bei allen teilnehmenden Cafés einlösbar', 'Bis zu 10 Kaffees auf den nächsten Monat übertragbar']
 
   return (
     <div className='py-16'>
@@ -13,21 +13,21 @@ const SubscriptionSection = () => {
           Unsere Abo-Modelle
         </h2>
         <p className="text-base text-[#433A21] text-center mb-4 max-w-4xl mx-auto">
-          Drei Abo-Modelle stehen dir zur Auswahl. Bei jedem Abo-Modell erhältst du eine unterschiedliche Anzahl an Credits pro Monat. Jedes dieser Credits kannst du gegen ein Kaffeegetränk deiner Wahl in teilnehmenden Cafés einlösen.
+          Drei Abo-Modelle stehen dir zur Auswahl. Bei jedem erhältst du eine unterschiedliche Anzahl an Kaffees pro Monat.
         </p>
         <div className="py-8 px-4 justify-stretch gap-y-8 md:gap-x-8 flex flex-col md:flex-row">
             <SubscriptionCard
-                title="Espresso"
+                title="Klein"
                 price="15 Euro / Monat"
                 features={espressoFeatures}
             />
             <SubscriptionCard
-                title="Cortado"
+                title="Mittel"
                 price="30 Euro / Monat"
                 features={cortadoFeatures}
             />
             <SubscriptionCard
-                title="Cappuccino"
+                title="Groß"
                 price="60 Euro / Monat"
                 features={cappuccinoFeatures}
             />
